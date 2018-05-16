c6e07889d6245a5ae0d95527037be39640cf7fcc
christophe panon <panon_chr@hotmail.com>
2018-05-16 16:51:04 -0600

    integrated index for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,11 @@
 class PortfoliosController < ApplicationController
+
+  def index
+    @portfolio_items = Portfolio.all
+  end
+
+
+
+
+
 end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,2 +1,3 @@
 class Portfolio < ApplicationRecord
+
 end
