c7dbd612d27881b0c1a946a23bfa088222be9a9f
christophe panon <panon_chr@hotmail.com>
2018-05-17 18:07:54 -0600

    added new and edit portfolio buttons

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -20,4 +20,23 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def show
+
+  end
+
 end
