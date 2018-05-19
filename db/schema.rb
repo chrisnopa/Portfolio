1b1c2677ac3787913194bb75916c58785327bcd7
christophe panon <panon_chr@hotmail.com>
2018-05-19 15:42:08 -0600

    corriged db schema with slug migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_16_204202) do
+ActiveRecord::Schema.define(version: 2018_05_18_020904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2018_05_16_204202) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
 
   create_table "portfolios", force: :cascade do |t|
