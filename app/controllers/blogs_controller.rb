04453c8a73521a8f4d5d2b84af91d283730ece20
christophe panon <panon_chr@hotmail.com>
2018-05-19 18:54:48 -0600

    done

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
 
   # GET /blogs
   # GET /blogs.json
@@ -59,6 +59,15 @@ class BlogsController < ApplicationController
     end
   end
 
+  def toggle_status
+    if @blog.published?
+      @blog.draft!
+    elsif @blog.draft?
+      @blog.published!
+    end
+    redirect_to blogs_url, notice:"Post status has been updated"
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
@@ -69,4 +78,4 @@ class BlogsController < ApplicationController
     def blog_params
       params.require(:blog,).permit(:title, :body)
     end
-end
+  end
