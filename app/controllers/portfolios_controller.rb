1c07d7f96dfdb6498f9451b8373faad033b57df5
christophe panon <panon_chr@hotmail.com>
2018-05-21 19:55:31 -0600

    implemented form config for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -8,10 +8,11 @@ class PortfoliosController < ApplicationController
   end
   def new
     @portfolio_item = Portfolio.new
+    3.times {@portfolio_item.technologies.build}
   end
 
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
 
     respond_to do |format|
       if @portfolio_item.save
