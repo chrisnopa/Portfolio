552cfd13ed63bb68e962996c251e0d83208d3d09
christophe panon <panon_chr@hotmail.com>
2018-05-24 18:03:54 -0600

    wip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
 
   include DeviseWhitelist
   include SetSource
+
+
+  def current_user
+    super || OpenStruct.new(name:"Guest User", first_name:"Guest", last_name:"User", email:"guest.test@test.fr")
+  end
 end
