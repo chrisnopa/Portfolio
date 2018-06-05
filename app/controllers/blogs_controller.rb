1875325f6fc7ed60782b58c683bc7c9851c5e39e
christophe panon <panon_chr@hotmail.com>
2018-06-05 09:00:08 -0600

    implemented pagination with kaminari gem

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,7 +6,7 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.all.order(created_at: :desc)
+    @blogs = Blog.page(params[:page]).per(5).order(created_at: :desc)
     @page_title = "My porfolio blogs"
   end
 
