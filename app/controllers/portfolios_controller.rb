9be0e26f01c0b43aeb50394179ccbe51d3534e2b
christophe panon <panon_chr@hotmail.com>
2018-06-11 21:17:26 -0600

    implemented initail file uplodagin image system via carrierwave for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -65,7 +65,7 @@ class PortfoliosController < ApplicationController
 
   def portfolio_params
 
-    params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name])
+    params.require(:portfolio).permit(:title, :subtitle, :body, :main_image, :thumb_image, technologies_attributes: [:name])
   end
 
   def set_portfolio_item
