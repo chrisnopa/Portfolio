30619f6c8ccec6f6b950cbe98c01f0a5a04be8fd
christophe panon <panon_chr@hotmail.com>
2018-08-01 20:09:44 +0200

    refactored alerts with Gritter for DRY

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -56,9 +56,15 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
-  def show_svg(path)
-    File.open("app/assets/images/#{path}", "rb") do |file|
-      raw file.read
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+    if alert
+      alert_generator alert
     end
   end
+
+  def alert_generator msg
+    js add_gritter(msg, title ="Christophe Panon Portfolio", sticky:false, time:1000)
+  end
+
 end
