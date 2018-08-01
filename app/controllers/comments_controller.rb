cc8094c8471a742e9b6d5b7574bfff5bc560c364
christophe panon <panon_chr@hotmail.com>
2018-08-01 21:59:00 +0200

    build out intial form and partal for comments etc

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+
+
+  def create
+    @comment = current_user.comments.build(comments_params)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
 end
