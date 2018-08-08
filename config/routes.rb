d79ba8b0f2bf358a51753b772df4dc4d4ab9fa3a
christophe panon <panon_chr@hotmail.com>
2018-08-08 17:40:34 +0200

    build out new feature for topics showing on index and show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :topics, only: [:index, :show]
   devise_for :users, path: '', path_names: {sign_in:'connexion', sign_out:'deconnexion', sign_up:"inscription"}
   resources :portfolios, except: [:show] do
     put :sort, on: :collection
