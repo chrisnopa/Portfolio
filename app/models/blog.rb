314a4d6844b498cd39d2c84bbeab55bd6efb7661
christophe panon <panon_chr@hotmail.com>
2018-08-11 23:01:03 +0200

    added approximated reading time to blog posts

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -8,4 +8,11 @@ class Blog < ApplicationRecord
   belongs_to :topic, optional:true
 
   has_many :comments, dependent: :destroy
+
+  def reading_time
+    words_per_minute = 150
+    text = Nokogiri::HTML(self.body).at('body').inner_text
+    (text.scan(/\w+/).length / words_per_minute).to_i
+  end
 end
+
